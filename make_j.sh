21c350a398ea90347fab77f26e32b01d13fd28f0
Alec Jacobson <alecjacobson@gmail.com>
2018-10-20 11:18:34 -0400

    lib --> igl

--- make_j.sh
+++ make_j.sh
@@ -4,6 +4,6 @@ for i in {1..20}
 do
   make clean &>/dev/null
   printf "$i  "
-  t=`(time make -j$i lib&>/dev/null) 2>&1 | grep real | sed -e "s/real[^0-9]*//g"`
+  t=`(time make -j$i igl&>/dev/null) 2>&1 | grep real | sed -e "s/real[^0-9]*//g"`
   echo "$t"
 done
